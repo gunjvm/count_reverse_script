4b6c6b71209c8048ffad83ee6c7aa6b9090c2fd5
Gunjan <gunjanvm7@gmail.com>
2024-08-11 01:35:09 -0400

    python script for count n reverse

---
+++ word_cnt_reverse.py
@@ -0,0 +1,25 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def test(s):
+    #write your code here
+    words = s.split()
+    
+    print(len(words))
+
+    for i in range(1, len(words), 2):
+        word = words[i]
+       
+        
+        if len(word) >= 2:
+            words[i] = word[-1] + word[1:-1][::-1] + word[0]
+        
+            result = ' '.join(words)
+    
+    print(result)    
+       
+    
+    return
+
+if __name__=='__main__':
+    
+    s=input()
+    (test(s))
